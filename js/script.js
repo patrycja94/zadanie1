1594ac0a36848ec22ed66eb5fdbf0175a951873c
patrycja94 <patrycja.w94@gmail.com>
2019-06-02 11:22:23 +0200

    zad3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    var gsearch = function() {
-        var url = 'https://www.google.com/search?q=' + document.getElementById('searchme').value;
+    let gsearch = function() {
+        let url = 'https://www.google.com/search?q=' + document.getElementById('searchme').value;
         window.open(url, 'google');
     };
-    var glucky = function() {
-        var url = 'https://www.google.com/search?q=' + document.getElementById('searchme').value + '&btnI';
+    let glucky = function() {
+        let url = 'https://www.google.com/search?q=' + document.getElementById('searchme').value + '&btnI';
         window.open(url, 'google');
     };
 
